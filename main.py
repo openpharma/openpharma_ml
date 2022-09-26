4c94be7c89d7378206d5354da0b85bbcbf15d42f
Mathieu Cayssol <mathieu.cayssol@gmail.com>
2022-09-26 19:54:03 +0200

    Add package categorization

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import prefect
 from prefect import task, Flow
-from models import search_engine, topic_modelling
+from models import package_categorization, search_engine
 import os
 
+KEYWORDS = "category_keywords.json"
 PATH_REPOS_CLEAN = "http://openpharma.s3-website.us-east-2.amazonaws.com/repos_clean.csv"
+PATH_ANNOTATED = "https://openpharma.s3.us-east-2.amazonaws.com/ml/repos_annotated.csv"
 PATH_SEARCH_TEST = "http://openpharma.s3-website.us-east-2.amazonaws.com/ml/query_test_set.csv"
 PATH_OPEN_ISSUES = "https://openpharma.s3.us-east-2.amazonaws.com/help_clean.csv"
 os.environ['AWS_ACCESS_KEY_ID'] = os.getenv('OPENPHARMA_AWS_ACCESS_KEY_ID')
@@ -16,13 +18,28 @@ with Flow("Search-engine Packages") as flow1:
     X_test_clean = search_engine.clean_data(X_test, is_lemma=False, remove_stop=False, is_alphabetic=True)
     X_embed = search_engine.inference_pretrained(X_clean)
     X_test_embed = search_engine.inference_pretrained(X_test_clean)
-
     search_engine.save_model(X_embed, key_id=os.getenv('AWS_ACCESS_KEY_ID'), access_key=os.getenv('AWS_SECRET_ACCESS_KEY'), file_name="inference_packages.pt")
-flow1.run()
+
 
 with Flow("Search-engine Open issues") as flow2:
     X = search_engine.openissues_read_csv(path_openissues=PATH_OPEN_ISSUES)
     X_clean = search_engine.clean_data(X, is_lemma=False, remove_stop=False, is_alphabetic=True)
     X_embed = search_engine.inference_pretrained(X_clean)
     search_engine.save_model(X_embed, key_id=os.getenv('AWS_ACCESS_KEY_ID'), access_key=os.getenv('AWS_SECRET_ACCESS_KEY'), file_name="inference_openissues.pt")
+
+
+with Flow("Package Categorization") as flow3:
+    df_anno = package_categorization.read_csv(path_csv=PATH_ANNOTATED)
+    df_repos = package_categorization.read_csv(path_csv=PATH_REPOS_CLEAN)
+    d = package_categorization.read_json(file_path=KEYWORDS)
+    # Data cleaning and prediction
+    df_repos = package_categorization.clean_data(df=df_repos)
+    df_repos = package_categorization.predict_category(df=df_repos, d=d)
+    # Merge inference and already annotated
+    df_repos = package_categorization.merge_repos_annotated(df_repos=df_repos, df_anno=df_anno)
+    package_categorization.save_dataframe(df=df_repos, key_id=os.getenv('AWS_ACCESS_KEY_ID'), access_key=os.getenv('AWS_SECRET_ACCESS_KEY'), file_name="repos_categorization.csv")
+
+
+flow1.run()
 flow2.run()
+flow3.run()
---
+++ models/package_categorization.py
@@ -0,0 +1,129 @@
+import pandas as pd
+from typing import Dict, List
+import json
+import spacy
+import ast
+import prefect
+from prefect import task
+import boto3
+
+@task
+def read_csv(path_csv: str):
+    df = pd.read_csv(path_csv)
+    return df
+
+@task
+def read_json(file_path: str)-> Dict:
+    with open(file_path, "r") as fi:
+        d = json.loads(fi.read())
+    return d
+
+
+def clean_text_nlp(X, is_lemma=True, remove_stop=True, is_alphabetic=True):
+    """
+    X : list of string such as ["sentence_1", "sentences_2", ... , "sentence_n"]
+    Return : list of list of words 
+    [
+        ["word_1", "word_2", ... , "word_n"], (sentence 1 cleaned)
+        ["word_1", "word_2", ... , "word_n"],
+        .
+        .
+        .
+        ["word_1", "word_2", ... , "word_n"] (sentence n cleaned)
+    ]
+    """
+    nlp = spacy.load("en_core_web_sm")
+    # (is_lemma = True, remove_stop = True, is_alpha = False) = (1,1,0)
+    hyperparam_tuple = (is_lemma, remove_stop, is_alphabetic)
+    new_X = []
+    while(True):
+        if((0,0,0)==hyperparam_tuple): # everything initialize with False
+            for doc in nlp.pipe(X, disable=['ner', 'parser', 'textcat']):
+                doc = [token.text.lower() for token in doc if(not(token.is_left_punct) and not(token.is_right_punct) and not(token.is_punct) and not(token.is_bracket))]
+                new_X.append(doc)
+            break
+        elif((1,0,0)==hyperparam_tuple): # lemma_
+            for doc in nlp.pipe(X, disable=['ner', 'parser', 'textcat']):
+                doc = [token.lemma_.lower() for token in doc if(not(token.is_left_punct) and not(token.is_right_punct) and not(token.is_punct) and not(token.is_bracket))]
+                new_X.append(doc)
+            break
+        elif((0,1,0)==hyperparam_tuple): # remove stop_words
+            for doc in nlp.pipe(X, disable=['ner', 'parser', 'textcat']):
+                doc = [token.text.lower() for token in doc if (not(token.is_stop) and not(token.is_left_punct) and not(token.is_right_punct) and not(token.is_punct) and not(token.is_bracket))]
+                new_X.append(doc)
+            break
+        elif((0,0,1)==hyperparam_tuple): # only alphabetic
+            for doc in nlp.pipe(X, disable=['ner', 'parser', 'textcat']):
+                doc = [token.text.lower() for token in doc if (token.is_alpha and not(token.is_left_punct))]
+                new_X.append(doc)
+            break
+        elif((1,1,0)==hyperparam_tuple): #lemma and remove stop_words
+            for doc in nlp.pipe(X, disable=['ner', 'parser', 'textcat']):
+                doc = [token.lemma_.lower() for token in doc if (not(token.is_stop) and not(token.is_left_punct) and not(token.is_right_punct) and not(token.is_punct) and not(token.is_bracket))]
+                new_X.append(doc)
+            break
+        elif((0,1,1)==hyperparam_tuple): #remove stop_words and only alphabetic
+            for doc in nlp.pipe(X, disable=['ner', 'parser', 'textcat']):
+                doc = [token.text.lower() for token in doc if (token.is_alpha and not(token.is_stop))]
+                new_X.append(doc)
+            break
+        elif((1,0,1)==hyperparam_tuple): #lemma and only alphabetic
+            for doc in nlp.pipe(X, disable=['ner', 'parser', 'textcat']):
+                doc = [token.lemma_.lower() for token in doc if(token.is_alpha and not(token.is_left_punct))]
+                new_X.append(doc)
+            break
+        elif((1,1,1)==hyperparam_tuple): #lemma and only alphabetic and remove stop_words
+            for doc in nlp.pipe(X, disable=['ner', 'parser', 'textcat']):
+                doc = [token.lemma_.lower() for token in doc if(token.is_alpha and not(token.is_stop))]
+                new_X.append(doc)
+            break
+        break
+
+    return new_X
+
+@task
+def clean_data(df: pd.DataFrame)-> pd.DataFrame:
+    df["description"].fillna("", inplace=True)
+    df["title"].fillna("", inplace=True)
+    df["description"] = df["title"] + " " + df["description"]
+    X = df["description"].to_list()
+    X_clean = clean_text_nlp(X, is_lemma=True, remove_stop=True, is_alphabetic=True)
+    df["description"] = X_clean
+    return df
+
+
+def binary_assignement(x: str, l: List)-> int:
+    return True if 1 in [1 if w in l else 0 for w in x] else False
+
+@task
+def predict_category(df: pd.DataFrame, d: Dict)-> pd.DataFrame:
+    df["plots_inf"] = df["description"].apply(lambda x: binary_assignement(x=x, l=ast.literal_eval(d["plots"])))
+    df["tables_inf"] = df["description"].apply(lambda x: binary_assignement(x=x, l=ast.literal_eval(d["tables"])))
+    df["stats_inf"] = df["description"].apply(lambda x: binary_assignement(x=x, l=ast.literal_eval(d["stats"])))
+    df["cdisc_inf"] = df["description"].apply(lambda x: binary_assignement(x=x, l=ast.literal_eval(d["cdisc"])))
+    df["utilities_inf"] = df["description"].apply(lambda x: binary_assignement(x=x, l=ast.literal_eval(d["utilities"])))
+    return df
+
+@task
+def merge_repos_annotated(df_repos: pd.DataFrame, df_anno: pd.DataFrame)-> pd.DataFrame:
+    df_repos = df_repos.merge(df_anno, how="left", on="full_name")
+    df_repos["plots"].fillna(df_repos["plots_inf"], inplace=True)
+    df_repos["tables"].fillna(df_repos["tables_inf"], inplace=True)
+    df_repos["stats"].fillna(df_repos["stats_inf"], inplace=True)
+    df_repos["cdisc"].fillna(df_repos["cdisc_inf"], inplace=True)
+    df_repos["utilities"].fillna(df_repos["utilities_inf"], inplace=True)
+    df_repos = df_repos[["full_name", "plots", "tables", "stats", "cdisc", "utilities"]]
+    return df_repos
+
+@task
+def save_dataframe(df: pd.DataFrame, key_id: str, access_key: str, file_name: str):
+    df.to_csv("{}.csv".format(file_name), index=False)
+    client = boto3.client('s3',
+        aws_access_key_id=key_id,
+        aws_secret_access_key=access_key
+    )
+    client.upload_file(Filename=file_name,
+        Bucket='openpharma',
+        Key='ml/{}'.format(file_name)
+    )
+    return 0
